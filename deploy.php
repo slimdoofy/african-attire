48eb3ed10f61bcf2691a822023f1d51e9d4d664d
Stanley Ahuchogu <info2stan@gmail.com>
2026-09-20 08:29:14 +0100

    deployment script

---
+++ deploy.php
@@ -0,0 +1,12 @@
+<?php
+    // Path to your repository
+    $repo_path = '/home/daceuabp/public_html/www.shopafricanattire.com';
+
+    // Pull latest changes
+    exec("cd {$repo_path} && git pull origin main", $output);
+
+    // Log output
+    file_put_contents('deploy.log', implode("\n", $output));
+
+    echo "Deployment Successful!";
+?>
